789f4efb3bec38629ba4ba1d6d20e0c4c4f23e6f
koduki <pascalm3-git@gmail.com>
2018-09-01 16:01:32 -0700

    Add support Babel

--- backend-app/src/index.js
+++ backend-app/src/index.js
@@ -1,22 +1,21 @@
+import * as Storage from "@google-cloud/storage"
+
 exports.helloGET = (req, res) => {
-  const Storage = require('@google-cloud/storage');
   const storage = new Storage();
 
   const file = storage
                 .bucket('cn_orz_pascal-bitcoin_prediction')
                 .file('btc_prediction.json');
 
-  console.log("kdebug2:" + file);
-
   file.download()
       .then(function(data){
-        if (data)
-            return data.toString('utf-8');
+        if (data) return data.toString('utf-8');
       })
       .then(function(data){
         if (data) {
-            console.log("get file "+file.name);
+            console.log("get file2 " + file.name);
             console.log(data);
+
             res.set('Access-Control-Allow-Origin', "*")
             res.set('Access-Control-Allow-Methods', 'GET, POST')
             res.writeHead(200, { 'Content-Type': 'application/json' });
